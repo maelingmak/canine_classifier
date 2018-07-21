27a8527182b8079f129b8b5441e6910fc6ae58f1
spinster <spinster@spinster.org>
2018-07-21 10:53:00 -0700

    updated again

--- app.py
+++ app.py
@@ -1,18 +1,65 @@
 import os
 from flask import Flask, render_template, jsonify, redirect
 from bson.objectid import ObjectId
-import ML_scott.py
+import find_dog_type_r1.py
+import io
+import keras
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from keras.applications.xception import (
+    Xception, preprocess_input, decode_predictions)
+from keras import backend as K
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'Uploads'
 
-@app.route('/')
-def index():
-	return render_template('index.html')
+model = None
+graph = None
 
-@app.route('/find_dog')
-def find_dog():
-    return {'dog_breed':dog_breed},
-           {'dog_jpg':dog_jpg}
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    data = {"success": False}
+    if request.method == 'POST':
+        if request.files.get('file'):
+            # read the file
+            file = request.files['file']
+
+            # read the filename
+            filename = file.filename
+
+            # create a path to the uploads folder
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            file.save(filepath)
+
+            # Load the saved image using Keras and resize it to the Xception
+            # format of 299x299 pixels
+            image_size = (299, 299)
+            im = keras.preprocessing.image.load_img(filepath,
+                                                    target_size=image_size,
+                                                    grayscale=False)
+
+            # preprocess the image and prepare it for classification
+            image = prepare_image(im)
+
+            global graph
+            with graph.as_default():
+                preds = model.predict(image)
+                results = decode_predictions(preds)
+                data["predictions"] = []
+
+                # loop over the results and add them to the list of
+                # returned predictions
+                for (imagenetID, label, prob) in results[0]:
+                    r = {"label": label, "probability": float(prob)}
+                    data["predictions"].append(r)
+
+                # indicate that the request was a success
+                data["success"] = True
+
+        return jsonify(data)
+
 
 if __name__ == "__main__":
-	app.run(debug = False)
+    app.run(debug=False)
+
